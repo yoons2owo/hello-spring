3b5da38f23d76a88e745667c2bd6c6bfcca90877
yoons2owo <yoons2owo@gmail.com>
2021-06-16 16:10:10 +0900

    API

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,19 +4,59 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
+    /*
+    컨트롤러에서 리턴 값으로 문자를 반환하면
+    뷰 리졸버( viewResolver )가 resource를 찾아서 템플릿 엔친 처리를 요청한다.
+     */
+
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "spring !");
-        return "hello"; // 컨트롤러에서 리턴 값으로 문자를 반환하면 뷰 리졸버( viewResolver )가 화면을 찾아서 처리한다.
+        return "hello"; //
     }
 
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model) {
-        model.addAttribute("name", name);   // viewResolver가 템플릿을 찾아서 타임리프 엔진 처리 요청
+        model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    /*
+    @ResponseBody 를 사용
+    - viewResolver 대신에 HttpMessageConverter 가 동작
+    - 기본 문자처리: StringHttpMessageConverter
+    - 기본 객체처리: MappingJackson2HttpMessageConverter(객체 -> json 변환)
+    - 클라이언트의 HTTP Accept 해더와 서버의 컨트롤러 반환 타입 정보 둘을 조합해서 HttpMessageConverter 가 선택된다
+    */
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
