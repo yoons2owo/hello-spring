1a5028eb3e7399c5ba94c2b5f8421653ae52e0b9
kekwon <64898042+kekwon@users.noreply.github.com>
2021-06-16 13:06:47 +0900

    프로젝트 환경설정
    프로잭트 생성~View 환경설정

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "spring !");
+        return "hello"; // 컨트롤러에서 리턴 값으로 문자를 반환하면 뷰 리졸버( viewResolver )가 화면을 찾아서 처리한다.
+    }
+}
