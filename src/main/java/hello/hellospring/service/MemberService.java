a3403a9787313f3a195d7453895c72c78352ac30
yoons2owo <yoons2owo@gmail.com>
2021-06-23 10:58:01 +0900

    회원 서비스 테스트 설명 추가

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,16 +2,21 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     // final : 값을 생성자에서 초기화 한 이후에 변경할 수 없다.
     private final MemberRepository memberRepository;
 
     // 외부에서 주입 - DI
+    // 기존에는 회원 서비스가 메모리 회원 레포를 직접 생성하게 했으나
+    // -> 회원 레포 코드가 회원 서비스 코드를 di 가능하게 변경한다
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
