c537ade2cdb1f5c29622b1cb21d5bf97a7f2e1ee
yoons2owo <yoons2owo@gmail.com>
2021-06-24 15:04:03 +0900

    스프링 빈과 의존관계 설명 추가

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -3,6 +3,7 @@ package hello.hellospring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// @SpringBootApplication 내부의 @ComponentScan 애노테이션이 현재 패키지 하위의 @Component를 스캔해서 스프링 컨테이너의  스프링 빈으로 등록해준다.
 @SpringBootApplication
 public class HelloSpringApplication {
 
--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -10,6 +10,7 @@ public class MemberController {
     private final MemberService memberService;
 
     // 생성자에 @Autowired가 있으면 스프링이 연관된 객체를 컨테이너에 찾아서 넣어준다.
+    // MemberController가 생성될 때 스프링 빈에 등록되어 있는 MemberService 객체를 넣어준다.
     // 이렇게 객체 의존관계를 외부에서 넣어주는 것을 DI(의존성 주입)라고 한다.
     @Autowired
     public MemberController(MemberService memberService) {
